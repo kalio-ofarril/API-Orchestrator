7b6543cf271a123f3aafc9f701093be5d85b9e2a
Kalio <kalio@Kalios-Mac-mini.local>
2025-05-22 21:54:59 -0600

    Creating mvn project

---
+++ src/main/java/com/apiorchestrator/API_Orchestrator/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.apiorchestrator.API_Orchestrator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiOrchestratorApplication.class);
+	}
+
+}
---
+++ src/test/java/com/apiorchestrator/API_Orchestrator/ApiOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiorchestrator.API_Orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
